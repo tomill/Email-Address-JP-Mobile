015cdc002ccb3355d393cd28a9a45e1e35b59821
Naoki Tomita <tomita@cpan.org>
2010-12-20 13:58:01 +0900

    add option for using utf-8 for NonMobile class

--- lib/Email/Address/JP/Mobile/NonMobile.pm
+++ lib/Email/Address/JP/Mobile/NonMobile.pm
@@ -3,6 +3,20 @@ use strict;
 use warnings;
 use base 'Email::Address::JP::Mobile::Base';
 
+our $Encoding    = 'iso-2022-jp';
+our $EncodingMap = {
+    'iso-2022-jp' => {
+        mime  => 'MIME-Header-ISO_2022_JP',
+        send  => 'iso-2022-jp',
+        parse => 'iso-2022-jp',
+    },
+    'utf-8' => {
+        mime  => 'MIME-Header',
+        send  => 'utf-8',
+        parse => 'utf-8',
+    },
+};
+
 sub matches {
     1;
 }
@@ -14,15 +28,15 @@ sub carrier_letter { 'N' }
 sub is_mobile { 0 }
 
 sub mime_encoding {
-    Encode::find_encoding('MIME-Header-ISO_2022_JP');
+    Encode::find_encoding($EncodingMap->{ $Encoding }{mime});
 }
 
 sub send_encoding {
-    Encode::find_encoding('iso-2022-jp');
+    Encode::find_encoding($EncodingMap->{ $Encoding }{send});
 }
 
 sub parse_encoding {
-    Encode::find_encoding('iso-2022-jp');
+    Encode::find_encoding($EncodingMap->{ $Encoding }{parse});
 }
 
 1;
